ca50e151ee58b1401eb9ad3934f2bd51937acb09
dhirajanand <dhiraj.na@gmail.com>
2021-04-30 07:51:33 +0530

    Integrating register with backend

---
+++ components/modals/ErrorModal.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Modal, Text, TouchableOpacity, View, Image } from "react-native";
+import { stringConstants, actionButtonTextConstants } from "../../constants/Constants";
+import { setErrorModal } from "../../helper/Helper";
+import { errorBoundaryStyles, SDGenericStyles } from "../../styles/Styles";
+
+export const ErrorModal = props => {
+
+    const { error, setError } = props;
+
+    return (
+        <Modal animationType="slide" transparent visible={error.showModal} onRequestClose={() =>
+            setErrorModal(error, setError, stringConstants.EMPTY, stringConstants.EMPTY, false)}>
+            <View style={errorBoundaryStyles.errorModalContainer}>
+                <View style={errorBoundaryStyles.modalViewStyle}>
+                    <View style={[SDGenericStyles.alignItemsCenter, SDGenericStyles.marginBottom4]}>
+                        <Text style={[errorBoundaryStyles.modalTitleTextStyle, SDGenericStyles.fontFamilyBold,
+                        SDGenericStyles.ft18]}>
+                            {error.title}
+                        </Text>
+                        <View style={errorBoundaryStyles.modalTitleDivider} />
+                    </View>
+                    <View style={errorBoundaryStyles.modalMessageViewStyle}>
+                        <Text style={[errorBoundaryStyles.modalMessageTextStyle, SDGenericStyles.fontFamilyRoman,
+                        SDGenericStyles.ft18]}>{error.message}</Text>
+                        <Image source={require(`../../assets/login_error_icon.png`)} style={errorBoundaryStyles.modalErrorImage} />
+                    </View>
+                    <TouchableOpacity activeOpacity={.3} style={errorBoundaryStyles.modalOKButtonStyle} onPress={() =>
+                        setErrorModal(error, setError, stringConstants.EMPTY, stringConstants.EMPTY, false)}>
+                        <Text style={[SDGenericStyles.colorWhite, SDGenericStyles.textCenterAlign, SDGenericStyles.ft18]}>
+                            {actionButtonTextConstants.OK}
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </Modal>
+    );
+};
